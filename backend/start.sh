2696f9cb6c912bc00639b4e4c270f6a14a4fd571
nlfernan <nlfernansam@gmail.com>
2025-07-27 16:32:24 -0400

    Deploy estructura escalable FastAPI

--- backend/start.sh
+++ backend/start.sh
@@ -1,24 +1,8 @@
-
-	
-	#!/bin/bash
-set -e
-
-echo "📂 Ruta actual:"
-pwd
-echo "📂 Archivos en contenedor:"
-ls -R
-
-echo "🔍 Verificando instalación de Panel y Bokeh..."
-python -c "import panel as pn; import bokeh; print('✅ Panel', pn.__version__, '✅ Bokeh', bokeh.__version__)"
-
-#echo "🔄 Ejecutando pipeline en background..."
-#python etl/pipeline.py &
-#PIPELINE_PID=$!
-#echo "✅ Pipeline corriendo en background con PID $PIPELINE_PID"
-
-echo "✅ Levantando FastAPI en background..."
-uvicorn app.main:app --host 0.0.0.0 --port 8000 &
-
 #!/bin/bash
 echo "🚀 Iniciando Dashboard de Fondos Mutuos con Streamlit..."
+
+# 👇 Si querés ejecutar el pipeline manualmente, descomentá la línea siguiente
+# python etl/pipeline.py
+
+# Levantar Streamlit
 streamlit run Dashboard_FFMM.py --server.port $PORT --server.address 0.0.0.0
