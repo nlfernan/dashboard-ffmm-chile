1a1925b1f6fd23bd11fe0b967739e5dda4e9d6f3
nlfernan <nlfernansam@gmail.com>
2025-08-07 22:53:49 -0400

    Deploy estructura escalable FastAPI

--- backend/start.sh
+++ backend/start.sh
@@ -2,7 +2,7 @@
 echo "🚀 Iniciando Dashboard de Fondos Mutuos con Streamlit..."
 
 # 👇 Si querés ejecutar el pipeline manualmente, descomentá la línea siguiente
-#python etl/pipeline.py
+python etl/pipeline.py
 
 # Levantar Streamlit
 streamlit run dashboard/app.py --server.port $PORT --server.address 0.0.0.0
